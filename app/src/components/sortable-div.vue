a372c8c4dfff370ebf4e102d5e3797b71bb06b48
nesb01t <nesb01t@163.com>
2023-11-16 20:06:19 +0800

    FEATURE sortable-div

--- app/src/components/sortable-div.vue
+++ app/src/components/sortable-div.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import Island from "./common/island.vue";
-import {computed} from "vue";
+import {computed, ref} from "vue";
+
+/**
+ * 0. BASIC
+ * 引入 @Vue use
+ * 初始化数据
+ * 随后通过 ref 挂载在某个元素
+ */
+import {useSortable} from '@vueuse/integrations/useSortable'
 
 const legacyList = ['1', '3', '12']
 const list = computed(() => {
@@ -13,13 +21,29 @@ const list = computed(() => {
     }
   })
 })
+const somethingToBeRef = ref<HTMLElement | null>(null)
+const {option} = useSortable(somethingToBeRef, list, {
+  /**
+   * 通过 useSortable 的 fn 参数直接设置 animation 属性
+   */
+  // animation: 500
+})
+
+/**
+ * 1. OPTION
+ * 通过选项添加动画等
+ */
+const ANIMATION_DURATION_MILLISECOND = 2000
+option('animation', ANIMATION_DURATION_MILLISECOND)
 </script>
 
 <template>
   <island>
     <h1>Sortable div</h1>
-    <div class="sortable-div">
-      <el-card v-for="(item) in list" :key="item" style="margin: 5px">{{item.name}}</el-card>
+    <div ref="somethingToBeRef" class="sortable-div">
+      <div v-for="(item) in list" :key="item.name" style="margin: 5px">
+        <el-card>{{ item.name }}</el-card>
+      </div>
     </div>
 
     <p>legacy value is: {{ JSON.stringify(legacyList) }}</p>
