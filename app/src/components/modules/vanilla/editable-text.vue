574f28d58e0666a135fbc4c2bec6e540ab3e7623
nesb01t <nesb01t@163.com>
2023-12-07 10:52:00 +0800

    update: editable-text

---
+++ app/src/components/modules/vanilla/editable-text.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import Island from "@/stories/Island.vue";
+import { CircleCheck, Edit } from "@element-plus/icons-vue";
+import { ref } from "vue";
+
+const editing = ref(false);
+const value = ref("default text");
+</script>
+
+<template>
+  <island>
+    <template v-if="editing">
+      <el-input v-model="value" style="" @blur="editing = false" />
+      <el-icon>
+        <CircleCheck />
+      </el-icon>
+    </template>
+    <span v-else class="editable-display" @click="editing = true">
+      <span class="editable-display-text">{{ value }}</span>
+      <el-icon class="editable-display-icon"><Edit /></el-icon>
+    </span>
+  </island>
+</template>
+
+<style scoped>
+.editable-display {
+  display: flex;
+  justify-items: center;
+}
+
+.editable-display-icon {
+  margin: auto 0 auto 5px;
+}
+</style>
